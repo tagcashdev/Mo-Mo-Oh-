bab195a0e26d157f2b039afb0c9f41eb12e117bb
André Fortuna <tagcashdev@gmail.com>
2022-02-08 11:06:03 +0100

    Clean Code
    Remove debug code
    Add new design for list card view

--- app/views/cards/list.php
+++ app/views/cards/list.php
@@ -1,23 +1,30 @@
 <?php $app = App::getInstance(); ?>
 
-<div>
+<div class="my-3">
     <div class="container">
         <div class="row g-3">
-            <?php foreach ($cards as $card) : ?>
-                <div>
-                    <?php
-                    echo '<pre>'; print_r($card); echo '</pre>';
+            <?php foreach ($cards as $card) :
 
-                    $color = "black";
-                    if($card->cards_tcg_release < $card->cards_ocg_release){
-                        $color = "green";
-                    }else{
-                        if($card->year_diff_tcg_ocg >= 2){ $color = "red"; }
-                    }
+                $color = "black";
+                if($card->cards_tcg_release < $card->cards_ocg_release){
+                    $color = "green";
+                }else{
+                    if($card->year_diff_tcg_ocg >= 2){ $color = "red"; }
+                }
 
-                     echo '<span style="color:'.$color.'">'.$card->date_diff_tcg_ocg.'</span>';
-                    ?>
+                ?>
+            <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img src="<?= $card->image_url ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">
+                        <span></span>
+                        <?= '<span style="color:'.$color.'">'.$card->date_diff_tcg_ocg.'</span>' ?>
+                    </p>
                 </div>
+            </div>
+            </div>
+
             <?php endforeach; ?>
         </div>
     </div>
